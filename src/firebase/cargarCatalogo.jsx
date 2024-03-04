465e3934bc6179bd902e40339b50fc0254921d48
Codivans <142336509+Codivans@users.noreply.github.com>
2024-03-04 12:53:01 -0600

    Update script update catalogo cargarCatalogo.jsx

--- src/firebase/cargarCatalogo.jsx
+++ src/firebase/cargarCatalogo.jsx
@@ -5,18 +5,18 @@ const cargarCatalogo = async (catalogo) => {
 
     await deleteDoc(doc(db, "catalogo", 'volante'));
 
-    const data = await catalogo.map(({codigo, nombre, precio, sustancia, departamento}) => {
+    const data = await catalogo.map(({codigo, nombre, precio, sustancia, oferta, departamento}) => {
         return (
             {
                 codigo: codigo,
                 nombre: nombre,
                 precio: precio,
+                oferta: oferta,
                 sustancia: sustancia,
                 departamento: departamento
             }
         )
     });
-    console.log(data);
 
     await setDoc(doc(db, "catalogo", 'volante'), {
         catalogo
